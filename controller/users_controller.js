bd3d93ee11cbf61b00c8f1ebfb7db20c718c5a07
Abubakar Aftab <abubakar.aftab786@gmail.com>
2021-02-15 13:08:51 +0530

    SignPages

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,6 +1,44 @@
+
+const User = require('../model/user');
 module.exports.profile = function(req, res){
     return res.render('user_page',{
         title: "Profile"
     });
 }
+// render the signup page
+module.exports.signup= function(req,res){
+    return res.render('user_sign_up',{
+        title: "Codeial- SignUp"
+    });
+}
+//render the signin page
+module.exports.signin= function(req,res){
+    return res.render('user_sign_in',{
+        title: "Codeial- SignIn"
+    });
+}
+//get the signup data
+module.exports.create = function(req, res){
+    if (req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
 
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('error in finding user in signing up'); return}
+
+        if (!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log('error in creating user while signing up'); return}
+
+                return res.redirect('/users/sign-in');
+            })
+        }else{
+            return res.redirect('back');
+        }
+
+    });
+}
+
+module.exports.createSession= function(req,res){
+    //todo
+}
--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 const usersController = require('../controller/users_controller');
-router.get('/profile',usersController.profile);
-module.exports= router;
-//aap jb tak view nhi create kroge yehi show hoga views folder me profile bnap
-//ohh ok
-// to aapne banadiya ye mai banao
-//sir aage sikhaynge 
-//but sir ka toh abhi se chalra
-//letme see
-//ok
+
+router.get('/profile', usersController.profile);
+
+router.get('/sign-up', usersController.signup);
+router.get('/sign-in', usersController.signin);
+//are you there?
+//yesotor?
+//its working?
+//thanks
+
+
+router.post('/create', usersController.create);
+
+
+module.exports = router;
