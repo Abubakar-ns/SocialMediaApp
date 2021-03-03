0da68cd53098226e1f677a217a652e80f2ed2eb7
Abubakar Aftab <abubakar.aftab786@gmail.com>
2021-03-03 15:55:53 +0530

    Authentication and Authorization using jwt to APIs

--- controller/api/v1/post_api.js
+++ controller/api/v1/post_api.js
@@ -19,13 +19,21 @@ module.exports.index= async function(req,res){
 module.exports.destroy = async function(req,res){
     try{
         let post= await Post.findById(req.params.id);
-        post.remove();
+        if(post.user == req.user.id){
+            post.remove();
 
-        await Comment.deleteMany({post: req.params.id});
+            await Comment.deleteMany({post: req.params.id});
 
-        return res.json(200,{
+            return res.json(200,{
             message: "Post and associated comments deleted succesfully"
-        });
+            });
+
+        }else{
+            return res.json(401,{
+                message: "You cannot delete this post!"
+            });
+        }
+        
         
     }catch(err){
         console.log("Error",err);
--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const postsApi = require('../../../controller/api/v1/post_api');
 
 router.get('/',postsApi.index);
-router.delete('/:id',postsApi.destroy);
+//session: false to not generate cookies 
+router.delete('/:id',passport.authenticate('jwt',{session: false}) ,postsApi.destroy);
 module.exports = router;
